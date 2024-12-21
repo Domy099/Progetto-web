e2ded538b4b51a3b8ab6391097f204e92b1318f3
alberto-romano03 <a.romano93@studenti.uniba.it>
2024-12-21 17:43:26 +0100

    Migliorato layout

--- app/components/GenericCard.jsx
+++ app/components/GenericCard.jsx
@@ -10,55 +10,37 @@ import Chip from '@mui/material/Chip';
 export default function GenericCard(props) {
   const { title, description, image, altText, tipo } = props;
   const truncatedDescription = description.length > 100 ? description.slice(0, 100) + '...' : description;
-  
+
   return (
-    <Card sx={{ maxWidth: 345, position: 'relative' }}>
-  <CardActionArea>
-    <CardContent sx={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
-      
-      {/* CardMedia per la locandina */}
-      <CardMedia
-        component="img"
-        image={image} // URL dell'immagine della locandina
-        alt={altText} // Alt text per l'immagine
-        sx={{
-          width: '100%', // Assicura che l'immagine prenda tutta la larghezza disponibile
-          height: 200, // Imposta l'altezza dell'immagine a 200px
-          objectFit: 'cover', // Scala l'immagine per coprire l'intero rettangolo senza deformarsi
-          borderRadius: '10px', // Arrotonda gli angoli dell'immagine
-          marginBottom: 2, // Distanza tra l'immagine e il resto del contenuto
-        }}
-      />
+    <Card sx={{ maxWidth: 345, position: 'relative', height: 400 }}>
+      <CardActionArea>
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', position: 'relative', height: '100%' }}>
 
-      {/* Chip sopra il titolo 
-      <Chip
-        label={tipo}
-        sx={{
-          alignSelf: 'flex-start', // Posiziona il chip a sinistra
-          backgroundColor: '#fdd835', // Colore giallo ocra
-          color: 'white', // Colore del testo bianco
-          marginBottom: 2, // Distanza dal titolo
-          marginTop: 2, 
-        }}
-      />
-      */}
-      
-      {/* Titolo e descrizione */}
-      <Typography gutterBottom variant="h5" component="div">
-        {title}
-      </Typography>
+          {/* CardMedia per la locandina */}
+          <CardMedia
+            component="img"
+            image={image} // URL dell'immagine della locandina
+            alt={altText} // Alt text per l'immagine
+            sx={{
+              width: '100%', // Assicura che l'immagine prenda tutta la larghezza disponibile
+              height: 250, // Imposta l'altezza dell'immagine a 200px
+              objectFit: 'cover', // Scala l'immagine per coprire l'intero rettangolo senza deformarsi
+              borderRadius: '10px', // Arrotonda gli angoli dell'immagine
+              marginBottom: 2, // Distanza tra l'immagine e il resto del contenuto
+            }}
+          />
 
-      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-        {truncatedDescription}
-      </Typography>
-    </CardContent>
-  </CardActionArea>
-</Card>
+          {/* Titolo e descrizione */}
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
 
+          <Typography variant="body2" sx={{ color: 'text.secondary', overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 3 }}>
+            {truncatedDescription} {/* Usa un testo troncato se la descrizione è troppo lunga */}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
-  
-  
-  
-  
-  
+
 }
