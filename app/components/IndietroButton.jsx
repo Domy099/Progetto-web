dfb52cda1c12514272a4ec2e649029d1ff93aa7c
alberto-romano03 <a.romano93@studenti.uniba.it>
2024-12-21 12:44:13 +0100

    Migliorato il click

--- app/components/IndietroButton.jsx
+++ app/components/IndietroButton.jsx
@@ -6,8 +6,8 @@ const BottoneIndietro = ({ destinazione }) => {
     const router = useRouter(); // Inizializzazione di useRouter
   
     return (
-      <Box display="flex" alignItems="center" gap={2} mt={2}>
-        <IconButton onClick={() => router.push(destinazione)} edge="start">
+      <Box display="flex" alignItems="center" gap={2} mt={2} onClick={() => router.push(destinazione)} edge="start">
+        <IconButton >
           <ArrowBackIcon />
         </IconButton>
         <Typography variant="h5" component="h1">
