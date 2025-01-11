d50d0a797dbffc118f21b57b6c3924e2e6f4ac6e
alberto-romano03 <a.romano93@studenti.uniba.it>
2025-01-11 18:30:20 +0100

    I bottoni nel menu hanno l'icon custom

--- app/components/ClickableCard.jsx
+++ app/components/ClickableCard.jsx
@@ -6,7 +6,7 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 const ClickableCard = ({ item }) => (
-  <Grid item xs={12} sm={6} md={4} key={item}>
+  <Grid item xs={12} sm={6} md={4} lg={3} key={item}>
     <Link href={`/${item.toLowerCase()}`} passHref>
       <Paper
         elevation={3}
@@ -16,40 +16,45 @@ const ClickableCard = ({ item }) => (
           borderRadius: '16px', // Angoli arrotondati
           overflow: 'hidden',
           position: 'relative',
+          width: '100%',
+          height: '200px', // Imposta un'altezza maggiore per ogni card
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
           '&:hover': {
             transform: 'scale(1.05)',
           },
         }}
       >
-        {/* Aggiungi l'SVG direttamente nel componente */}
         <div
           style={{
             position: 'absolute',
-            top: '-30px', // Posiziona l'SVG fuori dalla card
-            right: '-10px',
-            width: '60px',
-            height: '60px',
+            top: '0px', 
+            right: '-20px',
+            width: '80px',
             overflow: 'visible',
+            rotate: '-15deg',
+            
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100"
+          <img
+            src="./mask.svg"
+            alt="Mask"
             style={{
               position: 'absolute',
               top: '-20px',
               left: '-20px',
               width: '100px',
-              height: '100px',
-            }}
-          >
-            <circle cx="50" cy="50" r="50" fill="#EA580C" />
-          </svg>
+              height: '100px',            }}
+          />
         </div>
+
+
         <Typography variant="h6" className="text-black">{item}</Typography>
       </Paper>
     </Link>
   </Grid>
 );
+
 
 export default ClickableCard;
