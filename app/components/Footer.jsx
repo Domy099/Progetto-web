59ea98257aba1dde1d9683fd21bf8d361ec117dc
Vito <v.marchionna@studenti.uniba.it>
2025-01-26 19:06:40 +0100

    Sistemato footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,16 +1,47 @@
 "use client";
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/public/theme";
 
 const Footer = () => (
-  <Box
-    component="footer"
-    className="text-white p-4 mt-4 text-center"
-    sx={{ backgroundColor: '#408eb5', margin: 0, padding: 0 }}
-  >
-    <Typography variant="body1">&copy; 2024 Carnevale di Putignano. Tutti i diritti riservati.</Typography>
-  </Box>
+  <ThemeProvider theme={theme}>
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: "#d03526", // Colore rosa vivace dal tema
+        textAlign: "center",
+        padding: "24px 16px", // Spaziatura coerente
+        marginTop: "32px",
+        borderRadius: "8px 8px 0 0", // Bordo arrotondato in alto
+        boxShadow: "0 -4px 6px rgba(0, 0, 0, 0.1)", // Leggera ombra
+      }}
+    >
+      {/* Testo principale */}
+      <Typography
+        variant="body1"
+        sx={{
+          color: "#fff", // Testo bianco per contrasto
+        }}
+      >
+        &copy; 2025 Carnevale di Putignano. Tutti i diritti riservati.
+      </Typography>
+
+      {/* Testo secondario
+      <Typography
+        variant="body2"
+        sx={{
+          marginTop: "8px",
+          fontStyle: "italic", // Corsivo per aggiungere un tocco elegante
+          color: "#fff", // Testo bianco per contrasto
+        }}
+      >
+        Un tributo alla tradizione e alla gioia della Puglia.
+      </Typography>
+      */}
+    </Box>
+  </ThemeProvider>
 );
 
 export default Footer;
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useState } from "react";
 import Link from "next/link";
 import AppBar from "@mui/material/AppBar";
@@ -15,10 +15,9 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import Box from "@mui/material/Box";
 
-import CssBaseline  from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '@/public/theme';
-
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/public/theme";
 
 const Header = ({ menuItems }) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -55,11 +54,7 @@ const Header = ({ menuItems }) => {
             </IconButton>
             <Link href="/" passHref>
               <Typography variant="h6" className="flex-grow">
-                <img
-                  src= "/logoCarnevale.png"
-                  alt="Logo"
-                  className="h-10"
-                />
+                <img src="/logoCarnevale.png" alt="Logo" className="h-10" />
               </Typography>
             </Link>
           </div>
@@ -67,12 +62,18 @@ const Header = ({ menuItems }) => {
             {menuItems.map((item) => (
               <Link
                 key={item}
-                href={item.toLowerCase() === "home" ? "/" : `/${item.toLowerCase()}`}
+                href={
+                  item.toLowerCase() === "home" ? "/" : `/${item.toLowerCase()}`
+                }
                 passHref
               >
                 <Button color="inherit" className="menu-button">
-                  <Typography variant="body1" color="inherit" sx={{ textTransform: 'none' }}>
-                  {item}
+                  <Typography
+                    variant="body1"
+                    color="inherit"
+                    sx={{ textTransform: "none" }}
+                  >
+                    {item}
                   </Typography>
                 </Button>
               </Link>
@@ -87,78 +88,79 @@ const Header = ({ menuItems }) => {
       </AppBar>
 
       <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
-      <Box
-        sx={{
-          width: 256, // Larghezza del drawer
-          height: '100%',
-          bgcolor: 'background.paper', // Sfondo
-          p: 0, // Nessun padding esterno
-        }}
-        role="presentation"
-        onClick={toggleDrawer(false)}
-        onKeyDown={toggleDrawer(false)}
-      >
-        <List sx={{ marginTop: 4 }}>
-          {menuItems.map((text) => (
-            <ListItem 
-              key={text}
-              sx={{ width: '100%', paddingY: 0.5 }}
-            >
-              <Link
-                href={text.toLowerCase() === "home" ? "/" : `/${text.toLowerCase()}`}
-                passHref
-                style={{ textDecoration: 'none', width: '100%' }} // Link occupa tutta la larghezza
-              >
-                <ListItemButton
-                  sx={{
-                    width: '100%', // Espande il bottone
-                    textAlign: 'left', // Allineamento del testo
-                    padding: '12px 16px', // Padding interno
-                    borderRadius: 3,
-                    '&:hover': {
-                      background: '#c98ab1',
-                      color: '#ffffff',
-                      transition: 'all 0.2s ease-in-out',
-                    },
-                  }}
+        <Box
+          sx={{
+            width: 256, // Larghezza del drawer
+            height: "100%",
+            bgcolor: "background.paper", // Sfondo
+            p: 0, // Nessun padding esterno
+          }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List sx={{ marginTop: 4 }}>
+            {menuItems.map((text) => (
+              <ListItem key={text} sx={{ width: "100%", paddingY: 0.5 }}>
+                <Link
+                  href={
+                    text.toLowerCase() === "home"
+                      ? "/"
+                      : `/${text.toLowerCase()}`
+                  }
+                  passHref
+                  style={{ textDecoration: "none", width: "100%" }} // Link occupa tutta la larghezza
                 >
-                  <Typography variant="body1" color="inherit" sx={{ padding: 0.3 }}>
-                    {text}
-                  </Typography>
-                </ListItemButton>
-              </Link>
-            </ListItem>
-          ))}
-        </List>
-
-        
-        
-
-      </Box>
-      <div
-  style={{
-    position: 'absolute', // Posiziona il div in modo assoluto rispetto al contenitore
-    bottom: '0px', // Allinea il div al confine inferiore
-    right: '0px', // Allinea il div al confine destro
-    width: '100%', // Larghezza del div
-    height: '250px', // Altezza del div
-    overflow: 'hidden', // Nasconde la parte che eccede dal contenitore
-  }}
->
-  <img
-    src="/pattern-rombi.png"
-    style={{
-      position: 'absolute',  
-      width: '85%', // Rende l'immagine larga quanto il div
-      height: 'auto', // Mantiene le proporzioni dell'immagine
-      bottom: '-20px', 
-      right: '-50px',
-      opacity: 0.9,
-      transform: 'rotate(-15deg)', // Ruota l'immagine di -25 gradi
-    }}
-  />
-</div>
-    </Drawer>
+                  <ListItemButton
+                    sx={{
+                      width: "100%", // Espande il bottone
+                      textAlign: "left", // Allineamento del testo
+                      padding: "12px 16px", // Padding interno
+                      borderRadius: 3,
+                      "&:hover": {
+                        background: "#c98ab1",
+                        color: "#ffffff",
+                        transition: "all 0.2s ease-in-out",
+                      },
+                    }}
+                  >
+                    <Typography
+                      variant="body1"
+                      color="inherit"
+                      sx={{ padding: 0.3 }}
+                    >
+                      {text}
+                    </Typography>
+                  </ListItemButton>
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <div
+          style={{
+            position: "absolute", // Posiziona il div in modo assoluto rispetto al contenitore
+            bottom: "0px", // Allinea il div al confine inferiore
+            right: "0px", // Allinea il div al confine destro
+            width: "100%", // Larghezza del div
+            height: "250px", // Altezza del div
+            overflow: "hidden", // Nasconde la parte che eccede dal contenitore
+          }}
+        >
+          <img
+            src="/pattern-rombi.png"
+            style={{
+              position: "absolute",
+              width: "85%", // Rende l'immagine larga quanto il div
+              height: "auto", // Mantiene le proporzioni dell'immagine
+              bottom: "-20px",
+              right: "-50px",
+              opacity: 0.9,
+              transform: "rotate(-15deg)", // Ruota l'immagine di -25 gradi
+            }}
+          />
+        </div>
+      </Drawer>
     </ThemeProvider>
   );
 };
