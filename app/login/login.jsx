864919a933fbb12933a8b8d0aa92fa8a9c1e0cca
Vito <vito.marchionna@studenti.uniba.it>
2025-01-28 13:33:05 +0100

    Sistemazione completa di accesso e registrazione

--- app/login/login.jsx
+++ app/login/login.jsx
@@ -2,6 +2,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import "./login.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/public/theme";
+import { Typography } from "@mui/material";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -16,54 +20,29 @@ const Login = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    setError(""); // Reset previous errors
-  
+    setError("");
+
     try {
       const response = await axios.post(`${STRAPI_API_URL}/api/auth/local`, {
         identifier: email,
         password: password,
       });
-  
-      console.log("User profile:", response.data.user);
-      console.log("User token:", response.data.jwt);
-      console.log("Disattivato:", response.data.user.disattivato);
-  
-      // Recupera il valore di "disattivato"
-      
-  
+
       if (response.data.user.disattivato) {
-        alert("Utente disattivato. Se credi ci sia un errore, contatta l'amministratore: " + `${mailAssistenza}`);
+        alert(
+          "Utente disattivato. Se credi ci sia un errore, contatta l'amministratore: " +
+            `${mailAssistenza}`
+        );
         return;
       }
-  
+
       sessionStorage.setItem("token", response.data.jwt);
       router.push("/dashboard");
     } catch (err) {
-      setFailedAttempts(failedAttempts + 1); // Increment failed attempts
-      setError(err.response?.data?.message || "Errore. Credenziali non valide.");
-    }
-  };
-
-  const fetchUserData = async () => {
-    try {
-      const response = await fetch(
-        `${STRAPI_API_URL}/api/users/me`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
+      setFailedAttempts(failedAttempts + 1);
+      setError(
+        err.response?.data?.message || "Errore. Credenziali non valide."
       );
-  
-      if (!response.ok) {
-        throw new Error("Impossibile recuperare i dati utente");
-      }
-  
-      const userData = await response.json();
-      if (userData.disattivato) {
-        setDisattivato(true);
-      }
-    } catch (err) {
-      setError(err.message);
-      return null; // Gestisci l'errore restituito
     }
   };
 
@@ -80,99 +59,65 @@ const Login = () => {
   };
 
   return (
-    <div style={{ maxWidth: "400px", margin: "50px auto", padding: "20px", border: "1px solid #ddd", borderRadius: "5px", backgroundColor: "#f9f9f9" }}>
-      <h2 style={{ color: "black", fontWeight: "bold", textAlign: "left" }}>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div style={{ marginBottom: "15px" }}>
-          <label htmlFor="email" style={{ color: "#333", fontWeight: "bold", display: "block", textAlign: "center" }}>Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            placeholder="esempio@mail.com"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            style={{
-              width: "100%",
-              padding: "8px",
-              marginTop: "5px",
-              borderRadius: "4px",
-              border: "1px solid #ccc",
-              backgroundColor: "#fff",
-              color: "#000",
-            }}
-          />
-        </div>
-        <div style={{ marginBottom: "15px" }}>
-          <label htmlFor="password" style={{ color: "#333", fontWeight: "bold", display: "block", textAlign: "center" }}>Password</label>
-          <input
-            type={mostraPassword ? "text" : "password"}
-            id="password"
-            value={password}
-            placeholder="Password123"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            style={{
-              width: "100%",
-              padding: "8px",
-              marginTop: "5px",
-              borderRadius: "4px",
-              border: "1px solid #ccc",
-              backgroundColor: "#fff",
-              color: "#000",
-            }}
-          />
-          <button
-            type="button"
-            onClick={toggleMostraPassword}
-            style={{ marginTop: "5px", cursor: "pointer", background: "none", border: "none", color: "#007BFF" }}
-          >
-            {mostraPassword ? "Nascondi Password" : "Mostra Password"}
-          </button>
-        </div>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        {failedAttempts > 0 && (
-          <button
-            type="button"
-            onClick={handlePassReset}
-            style={{ marginTop: "10px", cursor: "pointer", background: "none", border: "none", color: "#007BFF" }}
-          >
-            Hai dimenticato la password? Reimposta qui.
-          </button>
-        )}
-        <button
-          type="submit"
-          style={{
-            width: "100%",
-            padding: "10px",
-            backgroundColor: "#408eb5",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
-        >
+    <ThemeProvider theme={theme}>
+      <div id="login-container">
+        <Typography variant="h2" id="login-title">
           Login
-        </button>
-        <button
-          type="button"
-          onClick={handleRegistrazione}
-          style={{
-            width: "100%",
-            padding: "10px",
-            marginTop: "10px",
-            backgroundColor: "#408eb5",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
-        >
-          Registrati
-        </button>
-      </form>
-    </div>
+        </Typography>
+        <form id="login-form" onSubmit={handleLogin}>
+          <div id="email-container">
+            <Typography variant="label">
+              Email
+            </Typography>
+            <input
+              type="email"
+              id="email-input"
+              value={email}
+              placeholder="esempio@mail.com"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div id="password-container">
+            <Typography variant="label">
+              Password
+            </Typography>
+            <input
+              type={mostraPassword ? "text" : "password"}
+              id="password-input"
+              value={password}
+              placeholder="Password123"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button
+              type="button"
+              id="toggle-password-btn"
+              onClick={toggleMostraPassword}
+            >
+              {mostraPassword ? "Nascondi Password" : "Mostra Password"}
+            </button>
+          </div>
+          {error && <p id="error-message">{error}</p>}
+          {failedAttempts > 0 && (
+            <button
+              type="button"
+              id="reset-password-btn"
+              onClick={handlePassReset}
+            >
+              Hai dimenticato la password? Reimposta qui.
+            </button>
+          )}
+          <button type="submit" id="login-btn">
+            Login
+          </button>
+          <button type="button" id="login-btn" onClick={handleRegistrazione}>
+            Registrati
+          </button>
+        </form>
+      </div>
+    </ThemeProvider>
   );
 };
 
-export default Login; 
+export default Login;
--- app/reset-password/page.jsx
+++ app/reset-password/page.jsx
@@ -2,6 +2,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import theme from "@/public/theme";
+import { ThemeProvider } from "@mui/material/styles";
+import "./reset.css";
+import { Typography } from "@mui/material";
 
 const ResetPassword = () => {
   const [email, setEmail] = useState("");
@@ -14,60 +18,50 @@ const ResetPassword = () => {
 
     try {
       // Richiesta per inviare il token di reset via email
-      const response = await axios.post("https://strapiweb.duckdns.org/api/auth/forgot-password", {
-        email: email,
-      });
+      const response = await axios.post(
+        "https://strapiweb.duckdns.org/api/auth/forgot-password",
+        {
+          email: email,
+        }
+      );
 
       console.log("Email inviata con successo:", response.data);
       alert("Email inviata con successo! Controlla la tua casella di posta.");
       router.push("/login"); // Reindirizza alla pagina di login
     } catch (err) {
-      setError(err.response?.data?.error?.message || "Errore durante l'invio della email.");
+      setError(
+        err.response?.data?.error?.message ||
+          "Errore durante l'invio della email."
+      );
     }
   };
 
   return (
-    <div style={{ maxWidth: "400px", margin: "50px auto", padding: "20px", border: "1px solid #ddd", borderRadius: "5px", backgroundColor: "#f9f9f9" }}>
-      <h2 style={{ color: "black", fontWeight: "bold", textAlign: "left" }}>Reimposta Password</h2>
-      <form onSubmit={handleResetPassword}>
-        <div style={{ marginBottom: "15px" }}>
-          <label htmlFor="email" style={{ color: "#333", fontWeight: "bold", display: "block", textAlign: "center" }}>Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            placeholder="esempio@mail.com"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            style={{
-              width: "100%",
-              padding: "8px",
-              marginTop: "5px",
-              borderRadius: "4px",
-              border: "1px solid #ccc",
-              backgroundColor: "#fff",
-              color: "#000",
-            }}
-          />
-        </div>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        <button
-          type="submit"
-          style={{
-            width: "100%",
-            padding: "10px",
-            backgroundColor: "#408eb5",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-          }}
-        >
-          Invia Email di Reset
-        </button>
-      </form>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div id="reset-password-box">
+        <Typography variant="h2">Reset Password</Typography>
+        <form onSubmit={handleResetPassword}>
+          <div>
+            <Typography variant="label" id="email-label">
+              Email
+            </Typography>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              placeholder="esempio@mail.com"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          {error && <p id="error-msg">{error}</p>}
+          <button type="submit" id="reset-password">
+            Invia Email di Reset
+          </button>
+        </form>
+      </div>
+    </ThemeProvider>
   );
 };
 
-export default ResetPassword; 
+export default ResetPassword;
